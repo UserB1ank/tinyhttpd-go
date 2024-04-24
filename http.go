19cb12ba207200fa41ff629303b14c9a08a96d34
UserB1ank <2577110290@qq.com>
2024-04-24 22:59:06 +0800

    基本实现了http报文处理中的获取url、method、headers

--- http.go
+++ http.go
@@ -3,19 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"strings"
 )
 
 // TODO 套接字、请求处理、执行cgi、多线程、进程通信
 func acceptRequest(conn net.Conn) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("socket write failed,error:", err)
+		}
+		return
+	}(conn)
 	reader := bufio.NewReader(conn)
-	var buf [1024]byte
-	var method [512]byte
-	len, err := reader.Read(buf[:])
+	//var data map[string]string
+	//data = make(map[string]string)
+	//var buf [1024]byte
+	//var method [512]byte
+	//length, err := reader.Read(buf[:])
+	//if err != nil {
+	//	fmt.Println("Read from client failed, error:", err)
+	//	return
+	//}
+	_, err := resolveHttp(reader, conn)
 	if err != nil {
-		fmt.Println("Read from client failed, error:", err)
+		fmt.Println(err)
 		return
 	}
+	//fmt.Println(data)
 
 }
 
@@ -27,17 +44,63 @@ func renderFile() {
 	//TODO 渲染网页文件
 }
 
-func readHeaders(conn net.Conn, len int) {
-	//TODO 读取http头
+/*处理报文，返回一个map，存储了http报文的method,URL,http版本(暂未获取),headers,body
+ */
+func resolveHttp(reader *bufio.Reader, conn net.Conn) (map[string]string, any) {
+	var data map[string]string
+	data = make(map[string]string)
+
+	//获取method,URL
+	line, err := reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	parts := strings.Fields(string(line))
+	if len(parts) < 2 {
+		//TODO 抛出 500 error
+		return nil, "server error 500"
+	}
+	data["method"] = parts[0]
+	data["url"] = parts[1]
+	data["headers"] = ""
+	data["body"] = ""
+	//获取headers
+	line, err = reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	for {
+		data["headers"] += string(line)
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		} else if string(line) == "\r\n" {
+			break
+		}
+	}
+
+	fmt.Printf("%q\n", data)
+	return data, nil
 }
 
-func readBody() {
-	//TODO 读取报文体
-}
-
-func readLine(conn net.Conn, len int) {
-	//TODO 读取一行http报文数据
-}
+//func readLine(reader *bufio.Reader) {
+//	//TODO 读取一行http报文数据
+//	//bytes, err := reader.ReadBytes('\n')
+//	//line, _, err := reader.ReadLine()
+//	line, _, err := reader.ReadLine()
+//	if err != nil {
+//		return
+//	}
+//	if err != nil {
+//		return
+//	}
+//	if err != nil {
+//		return
+//	}
+//	fmt.Printf("%q\n", string(line))
+//}
 
 func notFound() {
 	//TODO 404error
@@ -53,6 +116,7 @@ func main() {
 		fmt.Println("Listen failed, error:", err)
 		return
 	}
+	fmt.Println("start http: 8080")
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
